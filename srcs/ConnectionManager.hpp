fdb36861f22bb5649bf941aa921dc7d892365a3f
yanli <yanli@student.42.fr>
2025-09-19 15:24:38 +0200

    implemented ConnectionManager

--- srcs/ConnectionManager.hpp
+++ srcs/ConnectionManager.hpp
@@ -6,16 +6,39 @@
 /*   By: yanli <yanli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 14:17:33 by yanli             #+#    #+#             */
-/*   Updated: 2025/09/19 14:17:59 by yanli            ###   ########.fr       */
+/*   Updated: 2025/09/19 15:10:52 by yanli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONNECTIONMANAGR_HPP
 # define CONNECTIONMANAGER_HPP
 
+# include "_headers.hpp"
 
+class	EventLoop;
+class	Connection;
 
+/*	ConnectionManager carries all Connection objects
+	It creates new connections from accepted FDs;
+	It updates the record fd<---->Connection*;
+*/
+class	ConnectionManager
+{
+	private:
+		std::map<int,Connection*>	_conns;
 
-
+		ConnectionManager(const ConnectionManager &other);
+		ConnectionManager	&operator=(const ConnectionManager &other);
+	
+	public:
+		ConnectionManager(void);
+		~ConnectionManager(void);
+		
+		Connection	*establish(int client_fd, const std::string &server_name, EventLoop &loop);
+		void	drop(int fd);
+		void	drop_all(void);
+		Connection	*getConn(int fd) const;
+		size_t	getMapSize(void) const;
+};
 
 #endif
